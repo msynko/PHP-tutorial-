72dc8307afea4fb2d638c4c41202674e4269bba5
thinkingbox <thinkingbox@thinkingboxsmbp.lan>
2019-05-02 16:55:27 -0400

    Section three: Control Structures

---
+++ for_each.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+
+    <?php
+    
+    $numbers = [345, 690, 49203, 390];
+
+
+    foreach($numbers as $number){
+
+        echo $number . "<br";
+
+    }
+    
+    
+    ?>
+
+</body>
+</html>
---
+++ switch_stmt.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $number = 326;
+        switch($number){
+            case 34:
+            echo "is it 34?";
+            break;
+            case 35:
+            echo "is it 35?";
+            break;
+            case 36:
+            echo "is it 36?";
+            break;
+            case 37:
+            echo "is it 37?";
+            break;
+
+            default : 
+            echo "we'd find anything";
+        }
+    
+    
+    ?>
+</body>
+</html>
