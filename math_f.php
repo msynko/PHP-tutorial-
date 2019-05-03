ed3ac0767ba5a83ef3b273e90e00dfb22f57bd2b
thinkingbox <thinkingbox@thinkingboxsmbp.lan>
2019-05-03 13:35:42 -0400

    Section four: Buitd in functions

---
+++ math_f.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <?php
+    
+    echo pow(2,7); /// 2 in ^7
+    echo "<br>";
+    echo rand(1,10); //random number
+    echo "<br>";
+    echo sqrt(100); //10 
+    echo "<br>";
+    echo ceil(4.6); //round up to an integer
+    echo "<br>";
+    echo floor(5.7); //round down to an integer
+    ?>
+</body>
+</html> 
